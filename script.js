56e1726ffc700884ed963ad775c5e70dc5e54188
Nikit Singh Bisht <135353429+nikitbisht@users.noreply.github.com>
2024-05-12 15:10:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
+let mineContainer = document.querySelector(".mine-container");
 let mines = document.querySelectorAll('.box');
 const mode = document.querySelector('#modes');
-let mineContainer = document.querySelector(".mine-container");
-let matrix = [];
-let mrow = 6;
-let mcol = 6;
-updateMatrix();
-adjectCell();
+
+const flag = document.querySelector("#mine-count");
+const again = document.querySelector("#again");
+
 let loose = document.querySelector(".try-again");
 let win = document.querySelector(".play-again");
+
+let matrix = [];
+let visited = [];
+let mrow = 6;
+let mcol = 6;
+let flagCount = 6;
+
+
+updateMatrix();
+adjectCell();
+ModeChanging();
+
+//count no of bomb in matrix
+function countBomb () {
+	let cnt = 0;
+	for(let i=0; i<mcol ;i++){
+		for(let j=0; j<mrow; j++){
+			if(matrix[i][j] == -1)
+				cnt++;
+		}
+	}
+	return cnt;
+}
+
+//Create a new Grid
 function makeMatrix () {
 	for(let i =0; i<mrow*mcol; i++){
 		let element = document.createElement("div");
 		element.setAttribute("class","box");
 		mineContainer.appendChild(element);
 	}
-	blast();
+	mineClick();
 	adjectCell();
 }
 
@@ -26,7 +50,7 @@ function heightWidthSet (cName,val){
 		mine.classList.add(cName);
 	}
 }
-
+//Adjust grid cell height and width
 function adjectCell () {
 	if(mcol === 6){
 		heightWidthSet("easy",50);
@@ -42,48 +66,60 @@ function adjectCell () {
 function updateMatrix () {
 	mineContainer.innerHTML = "";
 	makeMatrix();
-	blast();
 	matrix=[];
+	visited =[];
 	create2dArray();
 	fillBombs();
+	SetFlag();
+	flagCount = countBomb();
+	flag.innerText = `${flagCount}`;
 }
 
 
-mode.addEventListener("click",(e)=>{
-	// console.log(e.target.value);
-	if(e.target.value === 'easy'){
-		mrow = 6;
-		mcol = 6;
-	}else if (e.target.value === 'medium') {
-		mrow = 8;
-		mcol = 8;
-	}else if (e.target.value === 'hard') {
-		mrow = 10;
-		mcol = 10;
-	}
-	updateMatrix();
-})
-
-
+function ModeChanging (argument) {	
+	let preValue = mode.value;
+	mode.addEventListener("click",(e)=>{
+		if(e.target.value==='easy' && e.target.value != preValue){
+			mrow = 6;
+			mcol = 6;
+			updateMatrix();
+		}else if (e.target.value==='medium' && e.target.value!=preValue) {
+			mrow = 8;
+			mcol = 8;
+			updateMatrix();
+		}else if (e.target.value==='hard' && e.target.value != preValue) {
+			mrow = 10;
+			mcol = 10;
+			updateMatrix();
+		}
+		preValue = mode.value;
+		handleWindow();
+	});
+}
 
 
 //Creation of 2d array
 function create2dArray() {
 	for(let i=0; i<mrow; i++){
 		let row = [];
+		let nrow = [];
 		for(let j=0; j<mcol; j++){
 			row.push(0);
+			nrow.push(0);
 		}
 		matrix.push(row);
+		visited.push(nrow);
 	}
 }
+
 
 //Random function
 function random(mini,maxi) {
 	return Math.floor(Math.random() * (maxi- mini + 1)) + mini;
 }
 
-//Fill Bombs
+
+//Fill Bombs in a grid
 function fillBombs() {
 	let mineCount = Math.floor(mines.length / 4);
 
@@ -95,37 +131,46 @@ function fillBombs() {
 	}
 }
 
-
-//Print to check
-function print() {
-	console.log("Array elemtn");
-	let cnt =0;
-	for(let i=0; i<mrow; i++){
-		console.log(`row ${i}`);
-		for(let j=0;j <mcol; j++){
-			console.log(matrix[i][j]);
-			cnt++;
-		}
-		console.log('\n');
-	}
-	console.log(cnt);
-}
-
-function blast () {
+//Check to revel mine
+function mineClick () {
 	mines = document.querySelectorAll('.box');
 	mines.forEach( (element, index) =>{
-		element.addEventListener("click",()=>{
-			// console.log(element)
+		element.addEventListener("click",(e)=>{
 			let row = Math.floor(index / mrow)
 			let col = index % mcol;
-			
-			solve(row,col);
-			print();
+			if(visited[row][col] != 1){
+				solve(row,col);
+			};
 		});
 	});
 }
 
 
+//Set the flag in gird
+function SetFlag () {
+	mines.forEach((element, index) =>{
+		element.addEventListener("contextmenu",(e)=>{
+			e.preventDefault();
+			let row = Math.floor(index / mrow)
+			let col = index % mcol;
+			if(visited[row][col] === 0 && flagCount>0){
+				flagCount --;
+				let newElement = document.createElement("img");
+				newElement.src="./images/flag.png";
+				newElement.setAttribute("class","flag-image");
+				visited[row][col] = 1;
+				element.appendChild(newElement);
+			}else if(visited[row][col] === 1){
+				flagCount++;
+				element.removeChild(element.firstChild);
+				visited[row][col] = 0;
+			}
+			flag.innerText = `${flagCount}`;
+		});
+	});
+}
+
+//Loose and blast All bomb
 function blastBom (row,col) {
 	let index ;
 	for(let i=0; i<mrow; i++){
@@ -138,7 +183,8 @@ function blastBom (row,col) {
 		}
 	}
 }
-//Count all mines in the matrix
+
+//Count all Unrevel & dont have bomb mines
 function unreavelMines () {
 	let Mcnt = 0;
 	for(let i=0; i<mrow; i++){
@@ -151,32 +197,26 @@ function unreavelMines () {
 	return Mcnt;
 }
 
-
+//Use DFS to traverse
 function solve (row,col) {
 	let index = col + mrow*row;
+	//Bomb
 	if(matrix[row][col] == -1){
 		blastBom(row,col);
-		console.log("End the game you lose");
 		setTimeout(()=>{
 			loose.parentElement.style= 'display:flex';
 		},1000);
 		return ;
 	}
-	if(unreavelMines() === 0){
-		blastBom(row,col);
-		setTimeout(()=>{
-			win.parentElement.style= 'display:flex';
-		},2000);
-	}
 
 
-	if(matrix[row][col] === 0){
-		let count = isSafe(row,col);
+	if(matrix[row][col] === 0 && visited[row][col] === 0){
+		let count = countNearBomb(row,col);
 
 		if(count == 0){
-			console.log("empty place");
 			mines[index].classList.add("empty");
 			matrix[row][col] = 99;
+			visited[row][col] = -1;
 
 			for(let drow=-1; drow<=1; drow++){
 				for(let dcol=-1; dcol<=1; dcol++){
@@ -189,6 +229,7 @@ function solve (row,col) {
 			}
 		}else{
 			matrix[row][col] = count;
+			visited[row][col] = 2;
 			if(count === 1){
 				mines[index].classList.add("green");
 			}else if(count === 2){
@@ -196,13 +237,21 @@ function solve (row,col) {
 			}else{
 				mines[index].classList.add("red");
 			}
-			mines[index].classList.add("bomb");
+			mines[index].classList.add("safe-place");
 			mines[index].textContent = count;	
 		}
 	}
+
+	if(unreavelMines() == 0){
+		blastBom(row,col);
+		setTimeout(()=>{
+			win.parentElement.style= 'display:flex';
+		},2000);
+	}
+
 }
 
-function isSafe (row,col) {
+function countNearBomb(row,col) {
 	let cnt = 0;
 	for(let drow=-1; drow<=1; drow++){
 		for(let dcol=-1; dcol<=1; dcol++){
@@ -217,13 +266,63 @@ function isSafe (row,col) {
 	return cnt;
 }
 
+
+
 //Loose Window
 loose.addEventListener(("click"),()=>{
 	loose.parentElement.style = "display:none"
 	updateMatrix();
+	handleWindow();
 })
+
+
 //Win window
 win.addEventListener(("click"),()=>{
 	win.parentElement.style = "display:none"
 	updateMatrix();
+	handleWindow();
 })
+
+
+
+//AGAIN
+again.addEventListener("click",(e)=>{
+	updateMatrix();
+	handleWindow();
+});
+
+
+//Remove Class to make grid smaller
+function removeClass(rclass) {
+	for(mine of mines){
+		mine.classList.remove(rclass);
+	}
+}
+
+
+//RESPONSIVE DESIGN
+const mediaQuery = window.matchMedia('(max-width:420px)');
+function handleWindow () {
+	if(mediaQuery.matches){
+		if(mcol === 6){
+			removeClass('easy');
+			heightWidthSet("Weasy",40)
+		}else if(mcol === 8){
+			removeClass('medium');
+			heightWidthSet("Wmedium",30);
+		}else if(mcol === 10){
+			removeClass('hard');
+			heightWidthSet("Whard",25);
+		}
+	}else{
+		removeClass('Wmedium');
+		removeClass('Whard');
+		removeClass('Weasy');
+		adjectCell();
+	}
+}
+
+mediaQuery.addListener(handleWindow);
+
+
+handleWindow();
